f3711238180cf7340b3361b0c83668d959a73f89
Morgan Leborgne <leborgne@zib.de>
2015-11-01 19:52:37 +0100

    installer: add a shortcut to /Application in the dmg

--- installer/osx/generate.sh
+++ installer/osx/generate.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-cd ../..
-
 qmake -spec macx-g++ -r "CONFIG+=release"
 
 make clean && make
 
-cd build
+(cd build && macdeployqt ShaderGen.app)
 
-macdeployqt ShaderGen.app -dmg -no-plugins
+mkdir dist
+cp -r build/ShaderGen.app License.txt dist
+rm -rf dist/ShaderGen.app/Contents/PlugIns/imageformats
+rm -rf dist/ShaderGen.app/Contents/PlugIns/printsupport
+ln -s /Applications dist/Applications
 
-mv ShaderGen.dmg ShaderGen-3.3.0.dmg
+hdiutil create \
+        -volname "ShaderGen" \
+        -srcfolder ./dist \
+        -ov \
+        ShaderGen-3.3.0.dmg
