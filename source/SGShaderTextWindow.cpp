1b01880403340fb695f7730db704b830f9653cd8
Morgan Leborgne <leborgne@zib.de>
2015-11-01 21:16:44 +0100

    SGShaderTextWindow: simplify the public interface.
    Remove the copyright since there is nothing left from the original code.

--- source/SGShaderTextWindow.cpp
+++ source/SGShaderTextWindow.cpp
@@ -1,40 +1,3 @@
-/************************************************************************
-*                                                                       *
-*               Copyright (C) 2002-2005  3Dlabs Inc. Ltd.               *
-*                                                                       *
-*                        All rights reserved.                           *
-*                                                                       *
-* Redistribution and use in source and binary forms, with or without    *
-* modification, are permitted provided that the following conditions    *
-* are met:                                                              *
-*                                                                       *
-*     Redistributions of source code must retain the above copyright    *
-*     notice, this list of conditions and the following disclaimer.     *
-*                                                                       *
-*     Redistributions in binary form must reproduce the above           *
-*     copyright notice, this list of conditions and the following       *
-*     disclaimer in the documentation and/or other materials provided   *
-*     with the distribution.                                            *
-*                                                                       *
-*     Neither the name of 3Dlabs Inc. Ltd. nor the names of its         *
-*     contributors may be used to endorse or promote products derived   *
-*     from this software without specific prior written permission.     *
-*                                                                       *
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   *
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     *
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     *
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE        *
-* COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, *
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,  *
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;      *
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER      *
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT    *
-* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN     *
-* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
-* POSSIBILITY OF SUCH DAMAGE.                                           *
-*                                                                       *
-************************************************************************/
-
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QPushButton>
@@ -44,10 +7,12 @@
 #include "SGCanvas.h"
 #include "QCodeEditor.h"
 
-SGShaderTextWindow::SGShaderTextWindow(SGFrame *frame):QFrame(frame)
+SGShaderTextWindow::SGShaderTextWindow(SGFrame *frame)
+    : QFrame(frame),
+      m_frame(frame),
+      m_haveCompiled(false),
+      m_haveLinked(false)
 {
-    m_frame = frame;
-
     notebook = new QTabWidget(this);
 
     textBoxVert = new QCodeEditor(notebook);
@@ -65,26 +30,24 @@ SGShaderTextWindow::SGShaderTextWindow(SGFrame *frame):QFrame(frame)
     notebook->addTab( textBoxFrag, "Fragment Shader");
     notebook->addTab( textBoxInfo, "InfoLog");
 
-    haveRefreshed = haveCompiled = false;
-
     QVBoxLayout *topSizer = new QVBoxLayout();
     QHBoxLayout *button_sizer = new QHBoxLayout();
 
     QPushButton *pb;
     pb = new QPushButton("1. GENERATE SHADERS", this);
-    connect(pb, SIGNAL(clicked()), SLOT(refresh()));
+    connect(pb, SIGNAL(clicked()), SLOT(generateClicked()));
     button_sizer->addWidget(pb);
 
     pb = new QPushButton("2. COMPILE", this);
-    connect(pb, SIGNAL(clicked()), SLOT(compile()));
+    connect(pb, SIGNAL(clicked()), SLOT(compileClicked()));
     button_sizer->addWidget(pb);
 
     pb = new QPushButton("3. LINK", this);
-    connect(pb, SIGNAL(clicked()), SLOT(link()));
+    connect(pb, SIGNAL(clicked()), SLOT(linkClicked()));
     button_sizer->addWidget(pb);
 
     pb = new QPushButton("BUILD", this);
-    connect(pb, SIGNAL(clicked()), SLOT(build()));
+    connect(pb, SIGNAL(clicked()), SLOT(buildClicked()));
     button_sizer->addWidget(pb);
 
     pb = new QPushButton("CLEAR INFO LOG", this);
@@ -97,53 +60,54 @@ SGShaderTextWindow::SGShaderTextWindow(SGFrame *frame):QFrame(frame)
     setLayout(topSizer);
 }
 
-void SGShaderTextWindow::compile()
-{
-    if(haveRefreshed)
-    {
-        haveCompiled = m_frame->getCanvas()->compileShaders(getVertexShaderText(), getFragmentShaderText());
-    }
-    notebook->setCurrentWidget(textBoxInfo);
-}
-
-void SGShaderTextWindow::link()
-{
-    if(haveCompiled)
-    {
-        haveLinked = m_frame->getCanvas()->linkShaders(getVertexShaderText(),getFragmentShaderText());
-        notebook->setCurrentWidget(textBoxInfo);
-        if(haveLinked)
-        {
-            m_frame->setCanvasMode(SGCanvas::GLModeChoiceShader);
-        }
-
-        textBoxInfo->textCursor().movePosition(QTextCursor::End);
-    }
-}
-
-void SGShaderTextWindow::build()
-{
-    refresh();
-    haveCompiled = m_frame->getCanvas()->compileShaders(getVertexShaderText(), getFragmentShaderText());
-    notebook->setCurrentWidget(textBoxInfo);
-    if(haveCompiled)
-    {
-        haveLinked = m_frame->getCanvas()->linkShaders(getVertexShaderText(), getFragmentShaderText());
-        if(haveLinked)
-        {
-            m_frame->setCanvasMode(SGCanvas::GLModeChoiceShader);
-        }
-
-        textBoxInfo->textCursor().movePosition(QTextCursor::End);
-    }
-}
-
-void SGShaderTextWindow::refresh()
+void SGShaderTextWindow::generateClicked()
 {
     textBoxFrag->setPlainText(m_frame->getFragmentShader());
     textBoxVert->setPlainText(m_frame->getVertexShader());
+}
 
-    haveRefreshed = true;
+void SGShaderTextWindow::compileClicked()
+{
+    const QString vert= m_frame->getVertexShader();
+    const QString frag= m_frame->getFragmentShader();
+
+    m_haveCompiled = m_frame->getCanvas()->compileShaders(vert, frag);
+
+    notebook->setCurrentWidget(textBoxInfo);
+}
+
+void SGShaderTextWindow::linkClicked()
+{
+    if(m_haveCompiled)
+    {
+        m_haveLinked = m_frame->getCanvas()->linkShaders(getVertexShaderText(),getFragmentShaderText());
+        notebook->setCurrentWidget(textBoxInfo);
+        if(m_haveLinked)
+        {
+            m_frame->setCanvasMode(SGCanvas::GLModeChoiceShader);
+        }
+
+        textBoxInfo->textCursor().movePosition(QTextCursor::End);
+    }
+}
+
+void SGShaderTextWindow::buildClicked()
+{
+    const QString vert= m_frame->getVertexShader();
+    const QString frag= m_frame->getFragmentShader();
+
+    m_haveCompiled = m_frame->getCanvas()->compileShaders(vert, frag);
+    notebook->setCurrentWidget(textBoxInfo);
+    if(m_haveCompiled)
+    {
+        m_haveLinked = m_frame->getCanvas()->linkShaders(vert, frag);
+        if(m_haveLinked)
+        {
+            m_frame->setCanvasMode(SGCanvas::GLModeChoiceShader);
+        }
+
+        textBoxInfo->textCursor().movePosition(QTextCursor::End);
+    }
 }
 
 void SGShaderTextWindow::log(const QString &text)
